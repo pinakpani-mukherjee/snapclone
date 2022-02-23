54ea09adf1fed55ae58d81baf59745e55e7b31eb
Pinakpani Mukherjee <mukherjee.pinakpani14@gmail.com>
2022-02-23 21:12:33 +0900

    added tab controller and firebase

---
+++ SnapClone/SignInVC.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  SnapClone
+//
+//  Created by Pinakpani Mukherjee on 2022/02/23.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    @IBOutlet weak var emailLabel: UITextField!
+    
+    @IBOutlet weak var nameLabel: UITextField!
+    
+    @IBOutlet weak var passwordLabel: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func signInClicked(_ sender: Any) {
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+    }
+    
+}
+
